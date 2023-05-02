c9666102900b2da6d718d1116640b6585e1eb274
Pavlo Navodych <grystme@gmail.com>
2023-05-02 13:19:16 +0300

    Create a myname app with Gradle

---
+++ src/main/java/User.java
@@ -0,0 +1,17 @@
+public class User {
+    private String name;
+    private String lastName;
+
+    public String getName() {
+        return name;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public User(String name, String lastName) {
+        this.name = name;
+        this.lastName = lastName;
+    }
+}
